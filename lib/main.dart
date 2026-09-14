ad9bb3d6a5349aecd7fbe46228cf15ba8e3013a2
Suresh <puresuresh@gmail.com>
2026-09-14 14:34:52 +0530

    Row & column code added

--- lib/main.dart
+++ lib/main.dart
@@ -24,11 +24,37 @@ class ExpenseHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Expense App'),
+        title: const Text('Expense App tese'),
       ),
-      body: const Center(
-        child: Text('Welcome to the Expense App!'),
+     /* body: const Center(
+        child: Text('Welcome to the Expense App test!'),
+        ), */
+     body: Card ( 
+      elevation: 5,
+      margin: EdgeInsets.all(10),
+      child : Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Column(
+          mainAxisSize: MainAxisSize.min,
+          children:[
+          Text("Groceries test",style: TextStyle(fontSize: 16),),
+          Text('September 15  2026',style: TextStyle(fontSize: 14)),
+          
+          ], 
+         ), 
+         Container(
+          child: Text("200.00",style: TextStyle(fontSize: 14,fontWeight:FontWeight.bold)),
+          decoration: BoxDecoration(
+          border: Border.all(color: Colors.black), borderRadius:BorderRadius.circular(10),),
+          padding:EdgeInsets.all(10),
+          margin: EdgeInsets.all(10),
+          ),
+        
+       
+        ],
       ),
-    );
+    ) );
   }
 }
